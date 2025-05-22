22b347cf0fb6cb9f79069e1dad8a3fb0bd6617c1
Marina <98844023+MarinaMassot@users.noreply.github.com>
2025-05-22 11:27:34 +0200

    install tailwind

--- src/Components/Checkbox.tsx
+++ src/Components/Checkbox.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+
 export const Checkbox = () => {
-	
+	const [checked, setChecked] = useState(false);
+  return <div>
+	<input type="checkbox" checked={false} onChange={() => {}}/>
+	</div>
+
 }
